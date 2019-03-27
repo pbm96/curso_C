f90cf5142b8be96ffe621b5162ee61b06133634a
Pablo <pbonillo96@gmail.com>
2019-03-27 18:52:43 +0100

    primer commit

---
+++ cmake-build-debug/CMakeFiles/aprendiendoC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aprendiendoC.dir/main.c.o"
+  "aprendiendoC.pdb"
+  "aprendiendoC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aprendiendoC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
